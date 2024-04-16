46fb7da3bb1bb7a6eb31f9bfe0d01f637aa9fd78
rrrrrzy <rzy1282950305@163.com>
2024-04-17 01:15:03 +0800

    实现了参数输入模式

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
-func filecut(file *os.File, size int64) {
-	fmt.Print("The size of file is ", size, "byte, file cut process is about to start...\n")
-
+func kwr_core(file *os.File, size int64, old_name, new_name string) {
 	content := make([]byte, size)
 
 	_, err := file.Read(content)
@@ -17,34 +17,41 @@ func filecut(file *os.File, size int64) {
 	}
 
 	s_content := string(content)
-	new_content := strings.ReplaceAll(s_content, "读博阁", "书包")
+	new_content := strings.ReplaceAll(s_content, old_name, new_name)
 	new_b := []byte(new_content)
 	new_file, err := os.Create("new_novel.txt")
 	if err != nil {
 		fmt.Print("create file error: ", err.Error())
 	}
 	new_file.Write(new_b)
+
 	fmt.Print("Success! New file has been created.")
 
 }
 
 func main() {
+	if len(os.Args) != 4 {
+		log.Fatal("Wrong PARAM Number!\nIt should be '$ kwr file.txt old_name new_name' ")
+	}
+	re, err := regexp.Compile(".txt")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	ok := re.MatchString(os.Args[1])
+	if !ok {
+		log.Fatal("Wrong type of filename! It should be like 'filename.txt'")
+	}
+
 	file, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Print("open file err: " + err.Error())
+		log.Fatal("open file err: " + err.Error())
 	}
 
 	stat, err := file.Stat()
 	if err != nil {
 		fmt.Print("get file status err: " + err.Error())
 	}
+
 	size := stat.Size()
-	if size < 10000 {
-		fmt.Printf("file size is: %v byte, do not need cut~~\n", size)
-		file.Close()
-		return
-	} else {
-		fmt.Print("file size is larger than 100kB, wait for cut!\n")
-		filecut(file, size)
-	}
+	kwr_core(file, size, os.Args[2], os.Args[3])
 }
